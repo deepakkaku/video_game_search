0a1d884cc9f01b122b949a9453afe73e8b23266f
Deepak Kaku <dkaku1@binghamton.edu>
2017-09-28 12:26:49 -0400

    Game entity POJO

---
+++ app/src/main/java/Custom/Game.java
@@ -0,0 +1,35 @@
+package Custom;
+
+/**
+ * Created by Deepak Kaku on 28-09-2017.
+ */
+
+public class Game {
+    private String title;
+    private String description;
+    private String image;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
