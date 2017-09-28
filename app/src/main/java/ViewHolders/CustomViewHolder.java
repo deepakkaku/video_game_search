029ca01381a98febbaf60e2ae2111953d8a727f3
Deepak Kaku <dkaku1@binghamton.edu>
2017-09-28 12:33:38 -0400

    new custom view holder

---
+++ app/src/main/java/ViewHolders/CustomViewHolder.java
@@ -0,0 +1,14 @@
+package ViewHolders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Deepak Kaku on 28-09-2017.
+ */
+
+public class CustomViewHolder extends RecyclerView.ViewHolder {
+    public CustomViewHolder(View itemView) {
+        super(itemView);
+    }
+}
