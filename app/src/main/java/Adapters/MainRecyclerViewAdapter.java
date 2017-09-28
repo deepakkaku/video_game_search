2ae4324403e977c6fff9769d8d80fa7ac8435175
Deepak Kaku <dkaku1@binghamton.edu>
2017-09-28 12:17:36 -0400

    new activity for details

---
+++ app/src/main/java/Adapters/MainRecyclerViewAdapter.java
@@ -0,0 +1,31 @@
+package Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Deepak Kaku on 28-09-2017.
+ */
+
+public class MainRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    public MainRecyclerViewAdapter(){
+
+    }
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
